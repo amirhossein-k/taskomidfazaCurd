9d8ddca5144373f43478b9e666679cfe5e26a9c0
amirhossein-k <amirhossein.k787878@gmail.com>
2025-07-20 00:43:07 +0330

    dcdvdv

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -3,28 +3,24 @@
 import Profile from '@/components/user/Profile';
 import { FetchUsersSingle } from '@/lib/axios/axiosRequest';
 import { notFound } from 'next/navigation';
-import { UserType } from '@/types/types';
+// import { UserType } from '@/types/types';
 
-type Props = {
+type PageProps = {
   params: {
     id: string;
   };
 };
 
-
-export default async function UserPage({ params }: Props) {
+export default async function UserPage({ params }: PageProps) {
   const { id } = params;
 
   try {
     const userData = await FetchUsersSingle(id);
+    if (!userData || !userData.data) notFound();
 
-    if (!userData || !userData.data) {
-      notFound();
-    }
-
-    return <Profile user={userData.data as UserType} />;
+    return <Profile user={userData.data} />;
   } catch (error) {
-    console.error('خطا در دریافت کاربر:', error);
+    console.error("خطا در دریافت کاربر:", error);
     notFound();
   }
 }
